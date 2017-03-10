0bd7f040ab57e08751050c29ec4429c33cc3fe50
deepakrkris <deepak.r.kris@gmail.com>
2017-03-10 10:39:18 -0800

    orchestrate from facade
    call account and customer services

--- services/facade/server/models/account.js
+++ services/facade/server/models/account.js
@@ -1,55 +1,34 @@
 'use strict';
 
-module.exports = function(Account) {
-  Account.getAccountSummary = function(accountNumber, cb) {
-    let Customer = Account.app.models.Customer;
+const app = require('../server');
 
-    Customer.dataSource.connector.client.Customer['Customer_find']({}, function(result) {
-      cb(null, result.obj);
-    });
+module.exports = function(Account) {
+  let accountService, customerService;
+
+  app.on('started', function() {
+    customerService = app.dataSources.Customer;
+    accountService = app.dataSources.Account;
+    customerService.getFunction = getFunction;
+    accountService.getFunction = getFunction;
+  });
+
+  function getFunction(model, method) {
+    let functionName = model + '_' + method;
+    return this.createModel(model, {})[functionName];
   }
 
-  Account.listAllAccounts = function(customerNumber) {
-    return Promise.resolve([
-      Math.random().toString().split('.')[1],
-      Math.random().toString().split('.')[1],
-      Math.random().toString().split('.')[1],
-      Math.random().toString().split('.')[1],
-      Math.random().toString().split('.')[1]
-    ]);
-  };
+  Account.getAccountSummary = function(accountNumber, cb) {
+    let findCustomer = customerService.getFunction('Customer', 'find');
+    let findAccount = accountService.getFunction('Account', 'find');
+    const accountSummary = {};
+    findCustomer({}, function(err, customer) {
+      if (err) return cb(err);
+      findAccount({}, function(err, account) {
+        if (err) return cb(err);
+        accountSummary.customer = customer.obj;
+        accountSummary.account = account.obj;
+        cb(null, accountSummary);
+      });
+    });
+  }
 };
-
-
-/*
-return Promise.resolve(new Account({
-      accountNumber: accountNumber,
-      accountInfo: {
-        avgBalance: 398.93,
-        minimumBalance: 10.00,
-        branch: 'Foster City',
-        type: 'Checking'
-      },
-      customerNumber: '0008893939',
-      customerInfo: {
-        firstName: 'Homer',
-        lastName: 'Simpson',
-        customerSince: new Date(),
-        address: {
-          street: '742 Evergreen Terrace',
-          lastUpdated: new Date(),
-          zip: 95555,
-          city: 'Springfield',
-          state: 'OR'
-        }
-      },
-      transactions: [
-        {
-          type: 'debit',
-          ammount: 3.93,
-          description: 'Starbucks'
-        }
-      ],
-      balance: 85.84
-    }));
-*/
