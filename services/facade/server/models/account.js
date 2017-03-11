8c299b4dbcb927da440742fe326b8bc995e77262
deepakrkris <deepak.r.kris@gmail.com>
2017-03-10 18:38:23 -0800

    Aggregate calls using bluebird
    add all results and return

--- services/facade/server/models/account.js
+++ services/facade/server/models/account.js
@@ -1,42 +1,33 @@
 'use strict';
 
 const app = require('../server');
+const promise = require('bluebird');
 
 module.exports = function(Account) {
-  let accountService, customerService, transactionService;
+  let customerService, accountService, transactionService;
 
-  app.on('started', function() {
-    customerService = app.dataSources.Customer;
-    accountService = app.dataSources.Account;
-    transactionService = app.dataSources.Transaction;
-    customerService.getFunction = getFunction;
-    accountService.getFunction = getFunction;
-    transactionService.getFunction = getFunction;
-  });
-
-  function getFunction(model, method) {
-    let functionName = model + '_' + method;
-    return this.createModel(model, {})[functionName];
-  }
-
-  Account.getAccountSummary = function(accountNumber, cb) {
-    let findCustomer = customerService.getFunction('Customer', 'findById');
-    let findAccount = accountService.getFunction('Account', 'findById');
-    let findAccountSummary = accountService.getFunction('AccountSummary', 'findById');
-    let findTransaction = transactionService.getFunction('Transaction', 'findById');
-    findAccountSummary({id: accountNumber, accountNumber: accountNumber}, function(err, accountSummary) {
-      if (err) return cb(err);
-      accountSummary = accountSummary.obj;
-      findAccount({id: accountNumber, accountNumber: accountNumber}, function(err, account) {
-        if (err) return cb(err);
-        findCustomer({id: account.obj.customerNumber, customerNumber: account.obj.customerNumber}, function(err, customer) {
-          if (err) return cb(err);
-          accountSummary.customer = customer.obj;
-          accountSummary.account = account.obj;
-          cb(null, accountSummary);
-        });
+  Account.getAccountSummary = function(accountNumber) {
+    let accountSummary = {};
+    return findAccountSummary({id: accountNumber, accountNumber: accountNumber})
+      .then(function(data) {
+        accountSummary = data.obj;
+        console.log("summary", accountSummary);
+      })
+      .then(function() {
+        return findAccount({id: accountNumber, accountNumber: accountNumber})
+      })
+      .then(function(data) {
+        accountSummary.account = data.obj;
+        console.log("account" , data.obj);
+      })
+      .then(function() {
+        return findCustomer({id: accountSummary.account.customerNumber, customerNumber: accountSummary.account.customerNumber})
+      })
+      .then(function(data) {
+        console.log(data);
+        accountSummary.customer = data.obj;
+        return accountSummary;
       });
-    });
   }
 
   Account.listAllAccounts = function(customerNumber, cb) {
@@ -46,4 +37,58 @@ module.exports = function(Account) {
       cb(null, account.obj);
     });
   };
+
+  app.on('started', function(){
+    customerService = app.dataSources.Customer;
+    accountService = app.dataSources.Account;
+    transactionService = app.dataSources.Transaction;
+    customerService.getFunction = getFunction;
+    accountService.getFunction = getFunction;
+    transactionService.getFunction = getFunction;
+  });
+
+  function findTransaction(input) {
+    let find = transactionService.getFunction('Transaction', 'findById');
+    return new Promise(function (resolve, reject) {
+      find(input, function(err, data) {
+        if (err) return reject(new Error(err));
+        resolve(data);
+      });
+    });
+  }
+
+  function findAccountSummary(input) {
+    let find = accountService.getFunction('AccountSummary', 'findById');
+    return new Promise(function (resolve, reject) {
+      find(input, function(err, data) {
+        if (err) return reject(new Error(err));
+        resolve(data);
+      });
+    });
+  }
+
+  function findAccount(input) {
+    let find = accountService.getFunction('Account', 'findById');
+    return new Promise(function (resolve, reject) {
+      find(input, function(err, data) {
+        if (err) return reject(new Error(err));
+        resolve(data);
+      });
+    });
+  }
+
+  function findCustomer(input) {
+    let find = customerService.getFunction('Customer', 'findById');
+    return new Promise(function (resolve, reject) {
+      find(input, function(err, data) {
+        if (err) return reject(new Error(err));
+        resolve(data);
+      });
+    });
+  }
+
+  function getFunction(model, method) {
+    let functionName = model + '_' + method;
+    return this.createModel(model, {})[functionName];
+  }
 };
