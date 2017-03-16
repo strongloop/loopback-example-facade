92fddeb73b1019201e79aa49dd1c0b0736fd7e3d
Simon Ho <simonho@ca.ibm.com>
2017-03-16 09:34:12 -0700

    Merge Account+AccountSummary models

--- services/facade/common/models/account.js
+++ services/facade/common/models/account.js
@@ -6,14 +6,9 @@ const Promise = require('bluebird');
 // NOTE: View `console.log` output in the docker-compose logs
 
 module.exports = function(Account) {
-  function getAggregateAccountSummary(accountNumber) {
-    const {Transaction} = app.models;
-    return Promise.props({
-      account: Account.getSummary(accountNumber),
-      customer: Account.getCustomer(accountNumber),
-      transactions: Transaction.find(accountNumber),
-    });
-  }
+  Account.getAccount = async function(accountNumber) {
+    return Account.Account_findById({id: accountNumber}).get('obj');
+  };
 
   Account.getAccountSummary = async function(accountNumber, cache) {
     const Cache = app.models.Cache;
@@ -21,7 +16,7 @@ module.exports = function(Account) {
     // DEMO(ritch): explain why url + acctNum instead of just acctNum
     const key = '/api/Account/summary?accountNumber=' + accountNumber;
 
-    console.log('checking the facade level cache');
+    console.log('checking facade level cache');
     let accountSummary = await Cache.get(key);
     if (accountSummary && cache !== false) {
       const ttl = await Cache.ttl(key);
@@ -30,30 +25,26 @@ module.exports = function(Account) {
       return accountSummary;
     }
 
-    console.log('cache miss, get data from microservice');
-    accountSummary = await getAggregateAccountSummary(accountNumber);
+    console.log('cache miss, retrieve data from microservices');
+    const {Transaction} = app.models;
+    accountSummary = await Promise.props({
+      account: Account.Account_findById({id: accountNumber}).get('obj'),
+      transactions: app.models.Transaction.find(accountNumber),
+    })
+    .then(accountSummary => {
+      const {Customer} = app.models;
+      accountSummary.customer = Customer.findById(accountSummary.account.customerNumber);
+      return Promise.props(accountSummary);
+    });
+
+    console.log('update cache with returned data');
     // ttl should be short because some data changes often
     // account aggressive infinite ttl at microservice level
     // customer aggresive infinite ttl
     // transaction non-aggressive very short ttl
-    console.log('update cache with returned data');
     // DEMO(ritch): change to 60s after, explain why 60s to crowd
     await Cache.set(key, accountSummary, {ttl: 60000}); // 10s for testing
 
     return accountSummary;
   };
-
-  Account.getAccountByNumber = function(accountNumber) {
-    return Account.Account_findById({id: accountNumber}).get('obj');
-  };
-
-  Account.getSummary = function(accountNumber) {
-    return Account.AccountSummary_findById({id: accountNumber}).get('obj');
-  }
-
-  Account.getCustomer = async function(accountNumber) {
-    const {Customer} = app.models;
-    const data = await Account.getAccountByNumber(accountNumber);
-    return Customer.findById(data.customerNumber);
-  }
 };
